4d2b260163f30a3b9e5170f2725caa884d6f632a
pakhyunw <hyunwoo.pak@m2cloud.kr>
2023-05-22 14:18:59 +0900

    ..

--- android/src/main/java/com/boskokg/flutter_blue_plus/FlutterBluePlusPlugin.java
+++ android/src/main/java/com/boskokg/flutter_blue_plus/FlutterBluePlusPlugin.java
@@ -394,8 +394,10 @@ public class FlutterBluePlusPlugin implements FlutterPlugin, MethodCallHandler, 
 
       case "pair":
       {
+        String BLE_PIN = "123456";
         String deviceId = (String)call.arguments;
         BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(deviceId);
+        device.setPin(BLE_PIN.getBytes());
         device.createBond();
         result.success(null);
         break;
