7d5e4701bfe747ed9029b4e7b8001b0a30e4fba2
pakhyunw <hyunwoo.pak@m2cloud.kr>
2023-06-07 14:50:42 +0900

    ..

--- android/src/main/java/com/boskokg/flutter_blue_plus/FlutterBluePlusPlugin.java
+++ android/src/main/java/com/boskokg/flutter_blue_plus/FlutterBluePlusPlugin.java
@@ -370,14 +370,18 @@ public class FlutterBluePlusPlugin implements FlutterPlugin, MethodCallHandler, 
 
           // If device was connected to previously but is now disconnected, attempt a reconnect
           BluetoothDeviceCache bluetoothDeviceCache = mDevices.get(deviceId);
-          if(bluetoothDeviceCache != null && !isConnected) {
+          while (bluetoothDeviceCache != null && !isConnected) {
+            if(bluetoothDeviceCache != null){
+              bluetoothDeviceCache.gatt.close();
+            }
             if(bluetoothDeviceCache.gatt.connect()){
               result.success(null);
             } else {
               result.error("reconnect_error", "error when reconnecting to device", null);
             }
-            return;
+            isConnected = mBluetoothManager.getConnectedDevices(BluetoothProfile.GATT).contains(device);
           }
+          return;
 
           // New request, connect and add gattServer to Map
           BluetoothGatt gattServer;
