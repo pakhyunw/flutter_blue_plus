7da994d2a51981cbc38f1154990b478a1b41c7aa
pakhyunw <hyunwoo.pak@m2cloud.kr>
2023-07-19 13:25:59 +0900

    ..

--- android/src/main/java/com/boskokg/flutter_blue_plus/AdvertisementParser.java
+++ android/src/main/java/com/boskokg/flutter_blue_plus/AdvertisementParser.java
@@ -131,8 +131,6 @@ class AdvertisementParser {
         }
       }
     } while (true);
-    int manufacturerId = data.getShort();
-    ret.putManufacturerData(manufacturerId+1, ByteString.copyFrom(rawData));
     return ret.build();
   }
 }
